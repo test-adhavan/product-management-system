239c71ec49beb3bc0de9aa101393ae2f5e974b64
test <test.adhavan@example.com>
2026-01-05 14:44:21 +0530

    Created service layer struture

---
+++ src/main/java/com/demo/product/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,6 @@
+package com.demo.product.service.impl;
+
+import com.demo.product.service.CategoryService;
+
+public class CategoryServiceImpl implements CategoryService {
+}
---
+++ src/main/java/com/demo/product/service/impl/ProductServiceImpl.java
@@ -0,0 +1,6 @@
+package com.demo.product.service.impl;
+
+import com.demo.product.service.ProductService;
+
+public class ProductServiceImpl implements ProductService {
+}
