2c004f15f0a1460fc69fea1b644cf3b50ebed250
test <test.adhavan@example.com>
2026-01-05 14:28:28 +0530

    created category and product entities with relationship

---
+++ src/main/java/com/demo/product/entity/Category.java
@@ -0,0 +1,21 @@
+package com.demo.product.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.AnyDiscriminatorImplicitValues;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id ;
+    private String name ;
+
+}
+
---
+++ src/main/java/com/demo/product/entity/Product.java
@@ -0,0 +1,35 @@
+package com.demo.product.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id ;
+    private String name ;
+    private Double price;
+    private Integer stock;
+    private Boolean active;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category_id;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+}
